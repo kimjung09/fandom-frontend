e55f6b164a8ee5a232a9c74bb292364b1a35c88c
wjdgh0727 <wjdgh0727@enterblock.co>
2021-11-04 16:43:57 +0900

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import React from "react";
+import React, {useState} from "react";
 import './App.css';
 import MainPage from "./components/Home";
 import SubPage from "./components/Info";
-import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import {Switch, Route, BrowserRouter} from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
 
-
 function App() {
+
   return (
       <>
           <Header/>
-      <Router>
+      <BrowserRouter>
           <Switch>
-              <Route exact path="/"  component={MainPage} />
-              <Route path="/:id" component={SubPage} />
+              <Route exact path="/" component={MainPage} />
+              <Route exact path="/shop/:id" component={SubPage} />
           </Switch>
-      </Router>
+      </BrowserRouter>
       <Footer />
       </>
   );
--- src/components/Home/Body/index.js
+++ src/components/Home/Body/index.js
@@ -1,8 +1,8 @@
-import React, {useState}from "react";
+import React, {useEffect, useState} from "react";
 import "./Body.css"
 import "./Story.css"
 import {FaPlay} from 'react-icons/fa'
-import {ItemList} from "../../item.json";
+import itemList  from '../../item.json';
 
 function Body() {
     const [clock, setClock] = useState("");
@@ -19,63 +19,72 @@ function Body() {
         setInterval(getTime, 1000);
     });
 
+    const [dataList, setDataList] = useState([]);
 
-   return (
-       <>
-      <div className="Body-container">
-          <div>
-              <form>
-                  <h1>own the most</h1>
-                  <h1>special moments!</h1>
-                  <p>own the most special moments!</p>
-                  <p className="Body-Components time">
-                      <span className="live">LIVE1</span>
-                      <span className="clock">{clock}</span>
-                  </p>
-                  <div>
-                      <div>
-                          <div className="Body-Video"></div>
-                      </div>
-                      <span >
+    useEffect(() => {
+        setDataList(itemList);
+    }, [])
+
+
+    return (
+        <>
+            <div className="Body-container">
+                <div>
+                    <form>
+                        <h1>own the most</h1>
+                        <h1>special moments!</h1>
+                        <p>own the most special moments!</p>
+                        <p className="Body-Components time">
+                            <span className="live">LIVE</span>
+                            <span className="clock">{clock}</span>
+                        </p>
+                        <div>
+                            <div>
+                                <div className="Body-Video"></div>
+                            </div>
+                            <span >
                           <FaPlay size={"20"}/>
                       </span>
-                  </div>
-              </form>
-          </div>
-      </div>
-           <div className="Story-container" id="Story">
-               <form className="Story-components">
-                   <div>
-                       <h1>
-                           1st Story
-                       </h1>
-                       <p>
-                           길거리에서 복권을 팔던 평범한 베트남 소녀, 국민 영웅이 되기까지 Nguyen Thi ThuNhi.
-                           <br/>
-                           그녀는 이제 하나의 계단만을 남겨두고 있습니다. 그녀의 WHO벨트 세계 타이틀전을 응원하며, 기념합니다.
-                       </p>
-                       <div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <div className="Story-container" id="Story">
+                <form className="Story-components">
+                    <div>
+                        <h1>
+                            1st Story
+                        </h1>
+                        <p>
+                            길거리에서 복권을 팔던 평범한 베트남 소녀, 국민 영웅이 되기까지 Nguyen Thi ThuNhi.
+                            <br/>
+                            그녀는 이제 하나의 계단만을 남겨두고 있습니다. 그녀의 WHO벨트 세계 타이틀전을 응원하며, 기념합니다.
+                        </p>
+                        <div>
+                            {
+                                dataList ? dataList.map((item, index) => (
+                                <React.Fragment key={index}>
+                                    <a href={`shop/${item.id}`}>
+                                        <img
+                                            key={item.id}
+                                            id={item.id}
+                                            src={item.img}
+                                        />
+                                    </a>
+                                </React.Fragment>
+                            )) : (
+                                    <div>등록된</div>
+                                )}
 
-                           {ItemList.map(item => (
-                               <React.Fragment key={item.id}>
-                               <a href={item.id}>
-                                   <img
-                                       key={item.id}
-                                       src={item.img}
-                                   />
-                               </a>
-                               </React.Fragment>
-                           ))}
-
-                       </div>
-                   </div>
-                   <button>
-                       bid now
-                   </button>
-               </form>
-           </div>
-       </>
-   )
+                        </div>
+                    </div>
+                    <button>
+                        bid now
+                    </button>
+                </form>
+            </div>
+        </>
+    )
 }
 
 export default Body;

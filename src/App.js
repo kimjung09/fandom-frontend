fd1ac63bce7bf7740c641bf7a81fa0ae549fa487
wjdgh0727 <wjdgh0727@enterblock.co>
2021-11-04 10:34:00 +0900

    init

--- src/App.js
+++ src/App.js
@@ -3,17 +3,22 @@ import './App.css';
 import MainPage from "./components/Home";
 import SubPage from "./components/Info";
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
 
 
 function App() {
   return (
       <>
+          <Header/>
       <Router>
           <Switch>
-                <Route exact path="/"  component={MainPage} />
-                <Route path="/:id" component={SubPage} />
+              <Route exact path="/"  component={MainPage} />
+              <Route path="/:id" component={SubPage} />
           </Switch>
       </Router>
+      <Footer />
       </>
   );
 }

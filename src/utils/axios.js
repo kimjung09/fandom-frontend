4d6cdc9c8ce57da28d9bac0550ce5ab78d078c3e
kiseokPark <kiseokPark@bitbucket.org>
2021-11-16 13:31:24 +0900

    update

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,9 +1,11 @@
 // axios
 import axios from 'axios'
 
+const url = process.env.REACT_APP_API_URL
+
 export const getNftList = async () => {
     try {
-        const response = await axios.get('http://3.35.194.80:8000/api/fandom');
+        const response = await axios.get(`${url}/fandom`);
         return response.data.data;
     } catch (e) {
         console.log(e);
@@ -13,7 +15,7 @@ export const getNftList = async () => {
 
 export const getNftInfo = async (id) => {
     try {
-        const response =await axios.get(`http://3.35.194.80:8000/api/fandom/${id}`);
+        const response =await axios.get(`${url}/fandom/${id}`);
         return response.data.data;
     } catch (e) {
         console.log(e);
